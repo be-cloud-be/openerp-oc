f6f7092ba84082e17b1d33a52b7f8c704690bb96
Jerome Sonnet <js@iMac-de-Jerome.local>
2014-11-08 08:39:27 +0100

    test

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(name='OpenERP',
+      version='7.0',
+      description='OpenERP on OpenShift',
+      author='Mateus Caruccio',
+      author_email='mateus.caruccio@getupcloud.com',
+      url='https://github.com/caruccio/openshift-openerp-quickstart',
+)
